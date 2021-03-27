c87fac4f836d48061c0647d8074feca8099ef42d
Andrey Takhtamirov <andrey1229@icloud.com>
2021-03-27 15:03:53 -0400

    Added DR process steps 2-4

--- DR/src/DR.c
+++ DR/src/DR.c
@@ -1,10 +1,12 @@
 #include "../inc/DR.h"
 
+int receiveMessage(MasterList *list, MESSAGE *msg);
 int checkIDinMasterList(MasterList list, int clientID);
 void displayMasterList(MasterList *list);
-void checkInactiveClients(MasterList *list);
+int checkInactiveClients(MasterList *list);
 void reorder(MasterList *list);
-
+int removeClient(MasterList *list, int clientIndex);
+int checkStatus(MasterList *list, MESSAGE msg);
 
 int main(void)
 {
@@ -78,7 +80,7 @@ int main(void)
     {
         printf("Cannot attach to shared memory!\n");
         // log
-        return 3;
+        return 1;
     }
 
     // init data to zeros
@@ -95,60 +97,80 @@ int main(void)
 
     printf("Message queue opened: %d\n", list->msgQueueID);
 
-    printf("sleeping for %d sec...\n", SECONDS_TO_SLEEP);
+    sleep(SECONDS_TO_SLEEP_ON_STARTUP);
 
-    sleep(SECONDS_TO_SLEEP);
-
-    printf("listening...\n");
+    printf("Listening...\n");
     fflush(stdout); // flush output to fix hang on sleep bug
 
     // listen for messages
     while (!done)
     {
-        // receive a message
-        recMsg = msgrcv(list->msgQueueID, (void *)&msg, sizeof(MESSAGE), 0, 0);
-        if (recMsg == NOT_FOUND)
+        if (receiveMessage(list, &msg) == NOT_FOUND)
         {
             printf("something bad happened!\n");
             // need to log
             break;
         }
 
-        printf("Received message: %d, status %d\n", msg.PID, msg.status);
-
-        // check if PID is new
-        int clientIndex = checkIDinMasterList(*list, msg.PID);
-        time_t seconds = time(NULL);
-
-        if (clientIndex == NOT_FOUND)
-        {
-            // client is new, add to MasterList
-            printf("adding PID %d to MasterList\n", msg.PID);
-            list->dc[list->numberOfDCs].dcProcessID = msg.PID;
-            list->dc[list->numberOfDCs].lastTimeHeardFrom = seconds;
-            list->numberOfDCs += 1;
-        }
-        else
-        {
-            // only update last heard from time
-            printf("updating PID  time %d\n", msg.PID);
-            list->dc[clientIndex].lastTimeHeardFrom = seconds;
-        }
-
-        displayMasterList(list);
-
         // check for inactive machines (more than 35 seconds has passed with no message)
-        checkInactiveClients(list);
+        // check for offline status
+        if (checkInactiveClients(list) == NOT_FOUND || checkStatus(list, msg) == NOT_FOUND)
+        {
+            printf("All DCs have gone offline or terminated - DR TERMINATING\n");
+            break;
+        }
 
         displayMasterList(list);
-
-
-        // if offline status, remove from master list and log offline.
 
         // sleep for 1.5 seconds.
+        sleep(SECONDS_TO_SLEEP_BETWEEN_MESSAGES);
     }
 
+    // clean up resources
+    printf("Detaching from Shared-Memory\n");
+    shmdt (list);
+
+    printf("Removing the Shared-Memory resource\n");
+    shmctl (sharedMemID, IPC_RMID, 0);
+
     return 0;
+}
+
+
+// receives a message
+int receiveMessage(MasterList *list, MESSAGE *msg)
+{
+    int recMsg;
+
+    // receive a message
+    recMsg = msgrcv(list->msgQueueID, (void *)msg, sizeof(MESSAGE), 0, 0);
+    if (recMsg == NOT_FOUND)
+    {
+        return NOT_FOUND;
+    }
+
+    printf("Received message: %d, status %d\n", msg->PID, msg->status);
+
+    // check if PID is new
+    int clientIndex = checkIDinMasterList(*list, msg->PID);
+    time_t seconds = time(NULL);
+
+    if (clientIndex == NOT_FOUND)
+    {
+        // client is new, add to MasterList
+        //printf("adding PID %d to MasterList\n", msg->PID);
+        list->dc[list->numberOfDCs].dcProcessID = msg->PID;
+        list->dc[list->numberOfDCs].lastTimeHeardFrom = seconds;
+        list->numberOfDCs += 1;
+    }
+    else
+    {
+        // only update last heard from time
+        //printf("client %d already exists, update time\n", msg->PID);
+        list->dc[clientIndex].lastTimeHeardFrom = seconds;
+    }
+
+    return SUCCESSFUL;
 }
 
 // check if ID is in MasterList, returning the index if contained, -1 else
@@ -182,29 +204,25 @@ void displayMasterList(MasterList *list)
 }
 
 
-void checkInactiveClients(MasterList *list)
+// checks all DCs for inactivity, removing inactive clients.
+int checkInactiveClients(MasterList *list)
 {
     time_t currentTime = time(NULL);
+    int i = 0;
 
-    for (int i = 0; i < list->numberOfDCs; i++)
+    for (i = 0; i < list->numberOfDCs; i++)
     {
         // if last heard from time is more than 35 seconds less than current time,
         // client is inactive
-        if(currentTime - list->dc[i].lastTimeHeardFrom > INACTIVE_CLIENT_TIME_SECONDS)
+        if (currentTime - list->dc[i].lastTimeHeardFrom > INACTIVE_CLIENT_TIME_SECONDS)
         {
-            // clear client from MasterList struct
-            list->dc[i].dcProcessID = 0;
-            list->dc[i].lastTimeHeardFrom = 0;
-
-            // collpase the MasterList struct (reorder to make all elements adjacent)
-            reorder(list);
+            return removeClient(list, i);
         }
     }
-    
-    
 }
 
 
+// reorders the MasterList (removing empty elements between DCs)
 void reorder(MasterList *list)
 {
     for (int i = 0; i < list->numberOfDCs; i++)
@@ -226,4 +244,48 @@ void reorder(MasterList *list)
             }
         }
     }
+}
+
+
+// checks the status of an incoming message and removes the DC if OFFLINE
+int checkStatus(MasterList *list, MESSAGE msg)
+{
+    int i = 0;
+
+    if (msg.status == MACHINE_OFFLINE)
+    {
+        // find and remove offline DC from MasterList
+        for (i = 0; i < list->numberOfDCs; i++)
+        {
+            if (list->dc[i].dcProcessID == msg.PID)
+            {
+                // this is our offline client
+                return removeClient(list, i);
+            }
+        }
+    }
+}
+
+
+// remove client of given index from MasterList
+int removeClient(MasterList *list, int clientIndex)
+{
+    printf("client removed %d\n", list->dc[clientIndex].dcProcessID);
+
+    // remove DC from MasterList struct
+    list->dc[clientIndex].dcProcessID = 0;
+    list->dc[clientIndex].lastTimeHeardFrom = 0;
+
+    // collpase the MasterList struct (reorder to make all elements adjacent)
+    reorder(list);
+
+    // update number of DCs
+    list->numberOfDCs -= 1;
+
+    if (list->numberOfDCs == 0)
+    {
+        return NOT_FOUND;
+    }
+
+    return SUCCESSFUL;
 }
